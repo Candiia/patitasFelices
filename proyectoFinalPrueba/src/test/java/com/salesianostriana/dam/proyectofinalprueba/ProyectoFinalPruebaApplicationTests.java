210f217fb0ff890fbd0fa0b1dd5eb71c575d98e0
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-04-14 19:41:01 +0200

    prueba

---
+++ proyectoFinalPrueba/src/test/java/com/salesianostriana/dam/proyectofinalprueba/ProyectoFinalPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectofinalprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
