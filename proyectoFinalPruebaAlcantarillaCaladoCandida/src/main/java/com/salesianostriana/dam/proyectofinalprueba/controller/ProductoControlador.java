a51041d49da00a18ef86f6a69d2ff3610bd7df6a
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-04-29 22:07:46 +0200

    comezar formulario de producto

--- proyectoFinalPruebaAlcantarillaCaladoCandida/src/main/java/com/salesianostriana/dam/proyectofinalprueba/controller/ProductoControlador.java
+++ proyectoFinalPruebaAlcantarillaCaladoCandida/src/main/java/com/salesianostriana/dam/proyectofinalprueba/controller/ProductoControlador.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.salesianostriana.dam.proyectofinalprueba.model.Animal;
@@ -41,11 +43,26 @@ public class ProductoControlador {
 		
 	}
 	
-
 	@GetMapping("/detalleAdminProducto")
 	public String detalleAdmin(Model model) {
 		List<Producto> listaProducto= productServ.findAll();
 		model.addAttribute("listaProductos", listaProducto);
 		return "pantallaAdminProducto";
 	} 
+
+	
+	@GetMapping("/formProducto")
+	public String formProducto(Model model) {
+		Producto producto = new Producto();
+		model.addAttribute("productoForm", producto);
+		return "formProducto";
+
+
+	}
+	
+	@PostMapping("/addProducto")
+	public String submit(@ModelAttribute("productoForm") Producto producto, Model model) {
+		model.addAttribute("producto", producto);
+		return "pantallaAdminProducto";
+	}
 }
