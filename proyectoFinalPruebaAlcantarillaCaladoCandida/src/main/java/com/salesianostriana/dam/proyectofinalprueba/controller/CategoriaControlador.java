dcc6f0b141304473146850099a03d9edda40c43e
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-05-06 19:23:55 +0200

    falta eliminar la categoria

--- proyectoFinalPruebaAlcantarillaCaladoCandida/src/main/java/com/salesianostriana/dam/proyectofinalprueba/controller/CategoriaControlador.java
+++ proyectoFinalPruebaAlcantarillaCaladoCandida/src/main/java/com/salesianostriana/dam/proyectofinalprueba/controller/CategoriaControlador.java
@@ -7,6 +7,7 @@ import com.salesianostriana.dam.proyectofinalprueba.model.Categoria;
 import com.salesianostriana.dam.proyectofinalprueba.service.CategoriaService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -32,6 +33,12 @@ public class CategoriaControlador {
 	@PostMapping("/agregarCategoria/submit")
 	public String submit(@ModelAttribute("categoria") Categoria categoria) {
 		catService.save(categoria);
+		return "redirect:/detalleCategoria";
+	}
+	
+	@GetMapping("/eliminarCategoria/{id}")
+	public String eliminar(@PathVariable("id") Long id) {
+		catService.deleteById(id);
 		return "redirect:/detalleCategoria";
 	}
 	
