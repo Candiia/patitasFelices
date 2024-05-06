81d69e358410d2ac75d1d5b2e13d61d409578a7b
Candi <Alcantarilla.cacan24@triana.salesianos.edu>
2024-05-06 12:07:37 +0200

    corregir errores

--- proyectoFinalPruebaAlcantarillaCaladoCandida/src/main/java/com/salesianostriana/dam/proyectofinalprueba/controller/AnimalControlador.java
+++ proyectoFinalPruebaAlcantarillaCaladoCandida/src/main/java/com/salesianostriana/dam/proyectofinalprueba/controller/AnimalControlador.java
@@ -1,18 +1,11 @@
 package com.salesianostriana.dam.proyectofinalprueba.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.salesianostriana.dam.proyectofinalprueba.model.Animal;
-import com.salesianostriana.dam.proyectofinalprueba.model.TipoAnimal;
-import com.salesianostriana.dam.proyectofinalprueba.repository.AnimalRepository;
 import com.salesianostriana.dam.proyectofinalprueba.service.AnimalService;
 import com.salesianostriana.dam.proyectofinalprueba.service.TipoAnimalService;
 
@@ -27,27 +20,21 @@ public class AnimalControlador {
 
 	@GetMapping("/mostrarAnimales")
 	public String todosProductos(Model model) {
-		List<Animal> listaAnimales= animalServ.findAll();
-		List<TipoAnimal> listaTipos = tipoServ.findAll();
-		model.addAttribute("listaAnimales", listaAnimales);
-		model.addAttribute("listaTipos", listaTipos);
+		
+		model.addAttribute("listaAnimales", animalServ.findAll());
+		model.addAttribute("listaTipos", tipoServ.findAll());
 		return "adoptar";
 	} 
 	
 	@GetMapping("/detalleAnimal")
 	public String detalleAnimal(@RequestParam Long id,  Model model) {
-		
-		Animal animal = animalServ.findById(id).get();
-		model.addAttribute("animal", animal);
+		model.addAttribute("animal",  animalServ.findById(id).get());
 		return "detalleAnimal";
 	}
 	
-
-	
 	@GetMapping("/detalleAdminAnimal")
 	public String detalleAdmin(Model model) {
-		List<Animal> listaAnimales= animalServ.findAll();
-		model.addAttribute("listaAnimal", listaAnimales);
+		model.addAttribute("listaAnimal", animalServ.findAll());
 		return "pantallaAdminAnimal";
 	} 
 	
