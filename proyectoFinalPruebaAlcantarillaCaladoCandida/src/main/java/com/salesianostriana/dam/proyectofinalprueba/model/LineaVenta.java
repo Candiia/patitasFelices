d756e2e8b2d165e1675656af0c16b895aa06f7cf
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-05-14 19:28:43 +0200

    Implementada la línea de venta de un producto

---
+++ proyectoFinalPruebaAlcantarillaCaladoCandida/src/main/java/com/salesianostriana/dam/proyectofinalprueba/model/LineaVenta.java
@@ -0,0 +1,32 @@
+package com.salesianostriana.dam.proyectofinalprueba.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Builder
+@IdClass(LineaVentaPK.class)
+public class LineaVenta {
+
+	@Id @GeneratedValue
+	private Long id;
+	
+	private int cantidad;
+	private double precioUnitario;
+	
+	@ManyToOne
+	private Producto producto;
+	
+	@Id
+	@ManyToOne
+	private Venta venta;
+}
